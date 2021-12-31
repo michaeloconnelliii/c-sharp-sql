e6bad6b653964f6ff6354837c1312d5f2bd3d78a
Michael O'Connell <michaeloconnelliii@gmail.com>
2021-12-30 16:50:29 -0800

    Finished chap 12 on joins

---
+++ sams/join/subquery-converted-to-join.sql
@@ -0,0 +1,7 @@
+/* Turn subquery example into a join for better
+   readability and efficiency */
+SELECT cust_name, cust_contact
+FROM Customers, Orders, OrderItems
+WHERE Customers.cust_id = Orders.cust_id
+AND OrderItems.order_num = Orders.order_num
+AND prod_id = 'RGAN01';
