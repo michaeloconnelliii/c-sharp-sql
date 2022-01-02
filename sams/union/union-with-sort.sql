aaa9daeb2c2ab44056cd9a0f039be34c2e1ade2a
Michael O'Connell <michaeloconnelliii@gmail.com>
2022-01-02 14:07:55 -0800

    Finished lesson 14 on Unions

---
+++ sams/union/union-with-sort.sql
@@ -0,0 +1,9 @@
+/* Union with Sorting Example */
+SELECT cust_name, cust_contact, cust_email
+FROM Customers
+WHERE cust_state IN('IL', 'IN', 'MI')
+UNION ALL
+SELECT cust_name, cust_contact, cust_email
+FROM Customers
+WHERE cust_name = 'Fun4All'
+ORDER BY cust_name, cust_contact;
