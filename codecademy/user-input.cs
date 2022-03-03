c734a17e2d1f10517572bcc9d0c406ca9c310507
Michael O'Connell <michaeloconnelliii@gmail.com>
2022-03-02 22:26:06 -0800

    Started C# tutorial

---
+++ codecademy/user-input.cs
@@ -0,0 +1,15 @@
+/* To run: $dotnet run */
+using System;
+
+namespace GettingInput
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("How old are you?");
+      string input = Console.ReadLine();
+      Console.WriteLine($"You are {input} years old!");
+    }
+  }
+}
