b30b0d5b87b01e9acb5ee7ba373f335326331699
Michael O'Connell <michaeloconnelliii@gmail.com>
2022-03-06 23:48:20 -0800

    c# tutorial

---
+++ ContosoCrafts/ContosoCrafts/Controller/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using ContosoCrafts.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoCrafts.WebSite.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get(
+            [FromQuery] string ProductId,
+            [FromQuery] int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+            return Ok();
+        }
+    }
+}
---
+++ ContosoCrafts/ContosoCrafts/Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿// Model for our cshtml page
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoCrafts.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoCrafts.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
+
+        public IndexModel(
+            ILogger<IndexModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        // When someone GETS this page, what do we do
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+
+        }
+    }
+}
